528a5c04d25b56341692d5cb5dd97daa3420b91e
Alexis Rieucau <alexis.rieucau@gmail.com>
2019-03-13 17:57:48 +0100

    added appcenter support

--- music_player_android/music_player_android/App.xaml.cs
+++ music_player_android/music_player_android/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace music_player_android
@@ -17,6 +20,10 @@ namespace music_player_android
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("android=e6eba985-a241-4ac3-b6c8-61e5e37d83d9;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
